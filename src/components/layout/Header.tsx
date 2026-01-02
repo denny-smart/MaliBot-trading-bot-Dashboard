3f39d39bb3ba313cf3cda0d95971b40b35d93ade
Denny-smart <ownerkirimi@gmail.com>
2026-01-02 09:45:57 +0000

    Changes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -22,7 +22,7 @@ export function Header({ title, onMenuClick }: HeaderProps) {
 
   const handleLogout = async () => {
     await logout();
-    navigate('/login');
+    navigate('/');
   };
 
   return (
